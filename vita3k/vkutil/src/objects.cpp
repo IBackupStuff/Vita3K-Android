bb80f892213c3379eab93a2b1567e934187ef5d5
CrmC <79812109+ikhoeyZX@users.noreply.github.com>
2026-04-27 02:33:30 +0800

    Update objects.cpp

--- vita3k/vkutil/src/objects.cpp
+++ vita3k/vkutil/src/objects.cpp
@@ -307,7 +307,7 @@ void DestroyQueue::destroy_objects() {
 #if defined(__aarch64__) || defined(__x86_64__)
             auto allocation = std::bit_cast<vma::Allocation>(destroy_list[idx++]);
 #else
-            auto allocation = reinterpret_cast<VmaAllocation>(static_cast<uint64_t>(destroy_list[idx++])
+            auto allocation = reinterpret_cast<VmaAllocation>(static_cast<uint64_t>(destroy_list[idx++]));
 #endif
             allocator.destroyImage(image, allocation);
             break;
